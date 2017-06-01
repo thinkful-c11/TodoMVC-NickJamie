9d35d5d17acfcc5c9978dc3074b0bb754a3920c9
Jamie Wang <sephona@gmail.com>
2017-06-01 11:17:14 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const express = require('express');
+const { DATABASE, PORT } = require('./config');
+
+const app = express();
+
+// Add middleware and .get, .post, .put and .delete endpoints
+app.get('/', (req, res) => {
+  res.send('hello world!');
+});
+
+app.get('/api/items', (req, res) => {
+  res.json([]);
+});
+
+let server;
+let knex;
+function runServer(database = DATABASE, port = PORT) {
+  return new Promise((resolve, reject) => {
+    try {
+      knex = require('knex')(database);
+      server = app.listen(port, () => {
+        console.info(`App listening on port ${server.address().port}`);
+        resolve();
+      });
+    }
+    catch (err) {
+      console.error(`Can't start server: ${err}`);
+      reject(err);
+    }
+  });
+}
+
+function closeServer() {
+  return knex.destroy().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing servers');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => {
+    console.error(`Can't start server: ${err}`);
+    throw err;
+  });
+}
+
+module.exports = { app, runServer, closeServer };
