cc0d0462b8f1996b6b3bae1e3eaf306dd73b4105
Jamie Wang <sephona@gmail.com>
2017-06-02 15:23:46 -0400

    switched out url

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'postgresql://dev:dev@localhost/todo-app';
+// const DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'postgresql://dev:dev@localhost/todo-app';
+
+const DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'postgres://hgvramvj:OTjHvLGC9yZm1iF47l9fpJmQEqUIbb-Q@stampy.db.elephantsql.com:5432/hgvramvj';
 
 exports.DATABASE = {
   client: 'pg',
